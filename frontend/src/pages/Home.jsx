35d5d227b34b66e128497c66caf704ee10d0d6be
jui2542123 <jui2542123@hotmail.com>
2022-05-13 17:10:15 +0700

    Adjust no room message

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -263,43 +263,6 @@ function Home(props) {
   //   const newTags = tags.filter((tag) => tag !== removedTag);
   //   setTags(newTags);
   // };
-
-  //testaginput2
-  // const TagsInput = props => {
-  //   const [tags, setTags] = React.useState([]);
-  //   const addTags = event => {
-  //     if (event.key === "Enter" && event.target.value !== ""){
-  //       setTags([...tags, event.target.value]);
-  //       props.selectedTags([...tags, event.target.value]);
-  //       event.target.value = "";
-  //     }
-  //   };
-  //   const removeTags = index => {
-  //     setTags([...tags.filter(tag => tags.indexOf(tag) !== index)]);
-  //   };
-  //   return (
-  //     <div className="tags-input">
-  //       <ul>
-  //         {tags.map((tag,index) => (
-  //           <li key={index}>
-  //             <span>{tag}</span>
-  //             <i className="material-icons"
-  //               onClick={() => removeTags(index)}
-  //             >
-  //               close
-  //             </i>
-  //           </li>
-  //         ))}
-  //       </ul>
-  //       <input
-  //         type="text"
-  //         onKeyUp={event => addTags(event)}
-  //         placeholder="Press enter to add tags"
-  //         />
-  //     </div>
-  //   );
-  // };
-
   
   /*{setAuth} */
   // const selectedTags = tags => console.log(tags);
@@ -365,7 +328,7 @@ function Home(props) {
           )}
         </div>
       ) : (
-        <div>
+        <div className="NoRoom">
           <p>No rooms found</p>
 		  {/* <button>test</button> */}
         </div>
