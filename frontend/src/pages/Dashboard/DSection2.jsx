623fcdf6c1d9a120591860f8a15a8004c9165bc2
jui2542123 <jui2542123@hotmail.com>
2022-05-24 00:47:17 +0700

    Update dashboard
    change graph to room members

--- frontend/src/pages/Dashboard/DSection2.jsx
+++ frontend/src/pages/Dashboard/DSection2.jsx
@@ -25,12 +25,6 @@ const Title = styled.h3`
 `;
 
 export default function DSection2({ title, data, dataKey, grid }) {
-  const [tags_count, setTagsCount] = useState(0);
-  const getTagsCount = async (e) => {
-    try {
-    } catch (err) {}
-  };
-
   return (
     <Container>
       <Chart>
